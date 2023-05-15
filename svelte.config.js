b2938abf7e571134ac0750c2e9f0063103fc4d0f
Don McCurdy <dm@donmccurdy.com>
2023-05-14 21:09:47 -0400

    Add highlight.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
-import adapter from '@sveltejs/adapter-static'
-import { mdsvex } from 'mdsvex'
-import preprocess from 'svelte-preprocess'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeSlug from 'rehype-slug'
+import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex';
+import preprocess from 'svelte-preprocess';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import hljs from 'highlight.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,34 +15,37 @@ const config = {
 			scss: {
 				// Ensures Sass variables are always available inside component <style> blocks as vars.$variableDefinedInFile
 				prependData: `@use 'src/lib/assets/scss/vars';`
-			},
+			}
 		}),
 		mdsvex({
 			// The default mdsvex extension is .svx; this overrides that.
 			extensions: ['.md'],
 
 			// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
-			rehypePlugins: [
-				rehypeSlug,
-				rehypeAutolinkHeadings,
-			],
-		}),
+			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+			highlight: {
+				highlighter: function (code, lang) {
+					const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+					return hljs.highlight(code, { language }).value;
+				}
+			}
+		})
 	],
 
 	kit: {
 		adapter: adapter(),
-    prerender: {
-      entries: [
-        '*',
-        '/api/posts/page/*',
-        '/blog/category/*/page/',
-        '/blog/category/*/page/*',
-        '/blog/category/page/',
-        '/blog/category/page/*',
-        '/blog/page/',
-        '/blog/page/*',
-      ]
-    }
+		prerender: {
+			entries: [
+				'*',
+				'/api/posts/page/*',
+				'/blog/category/*/page/',
+				'/blog/category/*/page/*',
+				'/blog/category/page/',
+				'/blog/category/page/*',
+				'/blog/page/',
+				'/blog/page/*'
+			]
+		}
 	}
 };
 
